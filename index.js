0328dabae4a32446e297655aa9ecc61c6ff606f5
Dave Wasmer <davewasmer@gmail.com>
2016-06-06 19:29:19 -0600

    initial commit, basic hello world server

---
+++ index.js
@@ -0,0 +1,13 @@
+let express = require('express');
+let consolidate = require('consolidate');
+
+let app = express();
+
+app.engine('hbs', consolidate.handlebars);
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(3000);
